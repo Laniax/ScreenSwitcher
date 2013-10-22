06ef69f4595019a8828b861b58cbcb8e8fd2d505
Laniax <bnoorlander@hotmail.com>
2013-10-22 14:55:41 -0700

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using ULTRAMONLib;
+
+namespace ScreenSwitcher
+{
+    public partial class MainForm : Form
+    {
+        private readonly NotifyIcon _trayIcon;
+        private readonly ContextMenu _trayMenu;
+        private readonly Hotkey _key;
+        private readonly IUltraMonSystem2 _system = new UltraMonSystem();
+
+        [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+        public static extern IntPtr GetForegroundWindow();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _trayMenu = new ContextMenu();
+            _trayMenu.MenuItems.Add("Switch", SwitchScreen);
+            _trayMenu.MenuItems.Add("Exit", OnExit);
+            _trayIcon = new NotifyIcon
+                            {
+                                Text = "ScreenSwitcher",
+                                Icon = new Icon(GetType(), "icon.ico"),
+                                ContextMenu = _trayMenu,
+                                Visible = true
+                            };
+
+            _key = new Hotkey { KeyCode = Keys.Escape, Shift = true };
+            _key.Pressed += delegate { SwitchScreen(null, null); };
+            _key.Register(); 
+        }
+
+        private void SwitchScreen(object sender, EventArgs e)
+        {
+            foreach (IUltraMonMonitor2 scr in _system.Monitors.Cast<object>().Where(scr => ((IUltraMonMonitor2)scr).CanChangeOrientation && Screen.FromHandle(GetForegroundWindow()).Primary == ((IUltraMonMonitor2)scr).Primary))
+            {
+                scr.Orientation = scr.Orientation == ORIENTATION.OR_PORTRAIT ? ORIENTATION.OR_LANDSCAPE : ORIENTATION.OR_PORTRAIT;
+
+                _system.ApplyMonitorChanges();
+            }
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            _key.Unregister();
+            _trayIcon.Dispose();
+            Dispose();
+            Application.Exit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+ScreenSwitcher
+==========
+
+A tiny application that will rotate your monitor 90° (and back).
+Build for a dualmonitor setup where the monitors have a pivot function (mainly for programming), more then 2 monitors not supported.
+
+This application will rotate the screen where your application has focus on, and is activated by the shortkey SHIFT+ESCAPE or rightclicking the context menu in the tray bar.
+
+Advised is to put a *shortcut* to the .exe in your Startup folder.
+
+Credits/Links
+------
+
+Changing of the display settings is done with the UltramonSDK (included in app).
+http://www.realtimesoft.com/ultramon/developers.asp
+
+The shortkey registering uses modified code from Max Bolingbroke
+http://bloggablea.wordpress.com/2007/05/01/global-hotkeys-with-net/
+
+.NET framework 3.5 is required to run this program. (included in Win7+ by default)
+http://www.microsoft.com/en-us/download/details.aspx?id=21
